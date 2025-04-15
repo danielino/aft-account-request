fe0f2cbadd52514f81ae991c2610d0e4dabcafff
Daniele Marcocci <daniele.marcocci@outlook.com>
2025-04-15 15:03:19 +0200

    feat: fix management ou

--- terraform/management.tf
+++ terraform/management.tf
@@ -8,7 +8,7 @@ module "management" {
     AccountEmail = "dmarcocci.aws.lab1@dxc.com"
     AccountName  = "root account"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "root"
+    ManagedOrganizationalUnit = "Root"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
     SSOUserEmail     = "dmarcocci.aws.lab1@dxc.com"
