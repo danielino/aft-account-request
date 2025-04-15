e84ebac31fa10c06ed2544dab3dca150538b1634
Daniele Marcocci <daniele.marcocci@outlook.com>
2025-04-15 14:34:55 +0200

    feat: add all accounts

--- terraform/logarchive.tf
+++ terraform/logarchive.tf
@@ -5,19 +5,19 @@ module "logarchive" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "xxxxxxxx"
-    AccountName  = "logarchive"
+    AccountEmail = "dmarcocci.aws.lab2@dxc.com"
+    AccountName  = "Log Archive"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Security"
+    ManagedOrganizationalUnit = "AWS Lab"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "do-not-use@example.com"
-    SSOUserFirstName = "do-not-use"
-    SSOUserLastName  = "do-not-use"
+    SSOUserEmail     = "dmarcocci.aws.lab1@dxc.com"
+    SSOUserFirstName = "Daniele"
+    SSOUserLastName  = "Marcocci"
   }
 
   account_tags = {
-    "ABC:Owner"       = "owner@example.com"
+    "ABC:Owner"       = "dmarcocci.aws.lab1@dxc.com"
     "ABC:Environment" = "Prod"
     "ABC:Project"     = "LandingZone"
   }
--- terraform/management.tf
+++ terraform/management.tf
@@ -5,19 +5,19 @@ module "management" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "xxxxxxxx"
-    AccountName  = "management"
+    AccountEmail = "dmarcocci.aws.lab1@dxc.com"
+    AccountName  = "root account"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "root"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "do-not-use@example.com"
-    SSOUserFirstName = "do-not-use"
-    SSOUserLastName  = "do-not-use"
+    SSOUserEmail     = "dmarcocci.aws.lab1@dxc.com"
+    SSOUserFirstName = "Daniele"
+    SSOUserLastName  = "Marcocci"
   }
 
   account_tags = {
-    "ABC:Owner"       = "owner@example.com"
+    "ABC:Owner"       = "dmarcocci.aws.lab1@dxc.com"
     "ABC:Environment" = "Prod"
     "ABC:Project"     = "LandingZone"
   }
